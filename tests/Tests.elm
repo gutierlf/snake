decfbda4655b1ea12fdbfbd794082503f9ef56d7
Luis F Gutierrez <luis@gutierrezhiller.com>
2017-05-23 13:47:41 -0700

    refactor names

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -11,16 +11,16 @@ all =
     describe "Model"
         [ test "Cranium starts at origin" <|
             \() ->
-                Expect.equal Snake.origin (.position Snake.cranium)
+                Expect.equal Snake.origin (.position Snake.initialModel)
         , test "Cranium start moving East" <|
             \() ->
-                Expect.equal Snake.East (.heading Snake.cranium)
+                Expect.equal Snake.East (.heading Snake.initialModel)
         , test "Move makes position == origin + (0, 1)" <|
             \() ->
-                Expect.equal (first Snake.origin + 1, second Snake.origin) (.position (Snake.move Snake.cranium))
+                Expect.equal (first Snake.origin + 1, second Snake.origin) (.position (Snake.move Snake.initialModel))
         , test "Move doesn't change the heading" <|
             \() ->
-                Expect.equal Snake.East (.heading (Snake.move Snake.cranium))
+                Expect.equal Snake.East (.heading (Snake.move Snake.initialModel))
         , test "North is the opposite of South" <|
             \() ->
                 Expect.equal Snake.North (Snake.opposite_heading Snake.South)
@@ -38,15 +38,15 @@ all =
                 Expect.true "Expected North to be opposite of South" (Snake.is_opposite_heading Snake.North Snake.South)
         , test "Turn North changes the heading to North" <|
             \() ->
-                Expect.equal Snake.North (.heading (Snake.turn Snake.North Snake.cranium))
+                Expect.equal Snake.North (.heading (Snake.turn Snake.North Snake.initialModel))
         , test "Turn East changes the heading to East" <|
             \() ->
-                Expect.equal Snake.East (.heading (Snake.turn Snake.East Snake.cranium))
+                Expect.equal Snake.East (.heading (Snake.turn Snake.East Snake.initialModel))
         , test "Turn South changes the heading to South" <|
             \() ->
-                Expect.equal Snake.South (.heading (Snake.turn Snake.South Snake.cranium))
+                Expect.equal Snake.South (.heading (Snake.turn Snake.South Snake.initialModel))
         , test "Turn West leaves heading at East" <|
             \() ->
-                Expect.equal Snake.East (.heading (Snake.turn Snake.West Snake.cranium))
+                Expect.equal Snake.East (.heading (Snake.turn Snake.West Snake.initialModel))
 
         ]
